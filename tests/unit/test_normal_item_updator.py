6d701a20ab43d422e51038c2a7906bfe8dbc500c
Vivek Santhirababu <vivek.santhirababu@kaizenreporting.com>
2023-11-26 22:49:21 +0000

    Further tests for normal item updater added, this includes test cases for negative quality

--- tests/unit/test_normal_item_updator.py
+++ tests/unit/test_normal_item_updator.py
@@ -15,3 +15,18 @@ def test_quality_degrades_twice_as_fast_after_sell_by_date():
     updater.update()
     assert item.quality == 18
     assert item.sell_in == -1
+
+
+def test_quality_never_negative():
+    item = Item("Normal Item", sell_in=10, quality=0)
+    updater = NormalItemUpdater(item)
+    updater.update()
+    assert item.quality == 0
+    assert item.sell_in == 9
+
+def test_quality_degrades_twice_as_fast_and_never_negative():
+    item = Item("Normal Item", sell_in=0, quality=1)
+    updater = NormalItemUpdater(item)
+    updater.update()
+    assert item.quality == 0
+    assert item.sell_in == -1
