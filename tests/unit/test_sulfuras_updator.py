54242100790d9a680382ecaca0e9b65952267c7c
Vivek Santhirababu <vivek.santhirababu@kaizenreporting.com>
2023-11-26 23:55:03 +0000

    Tests for sulfuras updator added

---
+++ tests/unit/test_sulfuras_updator.py
@@ -0,0 +1,19 @@
+import pytest
+from updaters.sulfuras_updater import SulfurasUpdater
+from gilded_rose import Item
+
+
+def test_sulfuras_quality_remains_constant():
+    quality = 80
+    item = Item("Sulfuras, Hand of Ragnaros", sell_in=10, quality=quality)
+    sulfuras_updater = SulfurasUpdater(item)
+    sulfuras_updater.update()
+    assert item.quality == quality, "Quality of 'Sulfuras' should remain constant"
+
+
+def test_sulfuras_sell_in_remains_constant():
+    sell_in = 10
+    item = Item("Sulfuras, Hand of Ragnaros", sell_in=sell_in, quality=80)
+    sulfuras_updater = SulfurasUpdater(item)
+    sulfuras_updater.update()
+    assert item.sell_in == sell_in, "SellIn of 'Sulfuras' should remain constant"
