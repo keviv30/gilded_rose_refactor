76a5b1c5206c9d67e13d988a4234976d39cca688
Vivek Santhirababu <vivek.santhirababu@kaizenreporting.com>
2023-11-26 22:45:54 +0000

    Normal item updater functionality added

---
+++ updaters/normal_item_updater.py
@@ -0,0 +1,15 @@
+from updaters import constants
+from updaters.abstract_item_updater import AbstractItemUpdater
+
+
+class NormalItemUpdater(AbstractItemUpdater):
+    def update(self):
+        self._decrease_sell_in()
+        decrease_amount = 2 if self.item.sell_in < 0 else 1
+        self._decrease_quality(amount=decrease_amount)
+
+    def _decrease_sell_in(self):
+        self.item.sell_in -= 1
+
+    def _decrease_quality(self, amount: int):
+        self.item.quality = max(constants.MIN_QUALITY, self.item.quality - amount)
