048bf20238d203bb95916b75afc06175f6536abe
Vivek Santhirababu <vivek.santhirababu@kaizenreporting.com>
2023-11-26 22:35:41 +0000

    Unit test for normal item updater added

---
+++ tests/unit/test_normal_item_updator.py
@@ -0,0 +1,17 @@
+import pytest
+from updaters.normal_item_updater import NormalItemUpdater
+from gilded_rose import Item
+
+def test_normal_item_quality_degrades():
+    item = Item("Normal Item", sell_in=10, quality=20)
+    updater = NormalItemUpdater(item)
+    updater.update()
+    assert item.quality == 19
+    assert item.sell_in == 9
+
+def test_quality_degrades_twice_as_fast_after_sell_by_date():
+    item = Item("Normal Item", sell_in=0, quality=20)
+    updater = NormalItemUpdater(item)
+    updater.update()
+    assert item.quality == 18
+    assert item.sell_in == -1
