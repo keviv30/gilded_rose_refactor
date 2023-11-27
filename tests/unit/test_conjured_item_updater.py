03540f9fed0aaaedd9984215d723287a59c96e31
Vivek Santhirababu <vivek.santhirababu@kaizenreporting.com>
2023-11-27 00:16:59 +0000

    Further tests for conjured items added, including tests for negative quality

--- tests/unit/test_conjured_item_updater.py
+++ tests/unit/test_conjured_item_updater.py
@@ -14,3 +14,17 @@ def test_quality_decreases_twice_as_fast_after_sell_by():
     updater = ConjuredItemUpdater(item)
     updater.update()
     assert item.quality == 6, "Quality should decrease by 4"
+
+
+def test_quality_never_negative():
+    item = Item("Conjured Mana Cake", sell_in=3, quality=1)
+    updater = ConjuredItemUpdater(item)
+    updater.update()
+    assert item.quality == 0, "Quality should not go below 0"
+
+
+def test_sell_in_decreases_each_day():
+    item = Item("Conjured Mana Cake", sell_in=5, quality=10)
+    updater = ConjuredItemUpdater(item)
+    updater.update()
+    assert item.sell_in == 4, "SellIn should decrease by 1"
