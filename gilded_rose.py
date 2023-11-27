aab3dee04fe5acf9f0874562ced61fc38732d36c
Vivek Santhirababu <vivek.santhirababu@kaizenreporting.com>
2023-11-27 00:39:23 +0000

    GildedRoss class updated with item updaters

--- gilded_rose.py
+++ gilded_rose.py
@@ -1,11 +1,39 @@
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+from typing import List, TypeVar, Generic, Type
+
+from updaters.abstract_item_updater import AbstractItemUpdater
+from updaters.aged_brie_updater import AgedBrieUpdater
+from updaters.backstage_pass_updater import BackstagePassUpdater
+from updaters.conjured_item_updater import ConjuredItemUpdater
+from updaters.normal_item_updater import NormalItemUpdater
+from updaters.sulfuras_updater import SulfurasUpdater
 
 
-class GildedRose(object):
-    def __init__(self, items):
+T = TypeVar('T', bound=AbstractItemUpdater)
+
+
+class GildedRose(Generic[T]):
+    def __init__(self, items: List[Item]):
         self.items = items
+        self.updater_map = {
+            "Aged Brie": AgedBrieUpdater,
+            "Backstage passes to a TAFKAL80ETC concert": BackstagePassUpdater,
+            "Sulfuras, Hand of Ragnaros": SulfurasUpdater,
+            "conjured": ConjuredItemUpdater
+        }
 
     def update_quality(self):
-        ...  # TODO
+        for item in self.items:
+            updater = self.get_updater(item.name)(item)
+            updater.update()
+
+    def get_updater(self, item_name: str) -> Type[T]:
+
+        if "conjured" in item_name.lower():
+            return self.updater_map["conjured"]
+
+        return self.updater_map.get(item_name, NormalItemUpdater)
 
 
 class Item:
