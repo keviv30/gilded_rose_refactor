6bd5a27dba257b285020cc2d5bbb992117a9f8f0
Vivek Santhirababu <vivek.santhirababu@kaizenreporting.com>
2023-11-27 00:24:55 +0000

    Further unit tests for AgedBrie added covering edge cases

--- tests/unit/test_aged_brie_updater.py
+++ tests/unit/test_aged_brie_updater.py
@@ -15,3 +15,24 @@ def test_aged_brie_quality_never_exceeds_50():
     updater.update()
     updater.update()  # Increasing twice to test the boundary
     assert item.quality == 50
+
+
+def test_aged_brie_sell_in_decreases():
+    item = Item("Aged Brie", 10, 20)
+    updater = AgedBrieUpdater(item)
+    updater.update()
+    assert item.sell_in == 9
+
+
+def test_aged_brie_quality_increases_twice_as_fast_after_sell_by():
+    item = Item("Aged Brie", 0, 20)
+    updater = AgedBrieUpdater(item)
+    updater.update()
+    assert item.quality == 22
+
+
+def test_aged_brie_quality_stops_at_50_after_sell_by():
+    item = Item("Aged Brie", 0, 49)
+    updater = AgedBrieUpdater(item)
+    updater.update()
+    assert item.quality == 50
